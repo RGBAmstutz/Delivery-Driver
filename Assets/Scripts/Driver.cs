eafdda8d5042c250f36722aeca3445c2ad5b8339
Ryan Amstutz <59939000+Xaelonous@users.noreply.github.com>
2021-12-20 00:47:18 -0500

    added pickups for slowing and speeding up as well as bumping into things slows

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -2,22 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Driver : MonoBehaviour
 {
 
     [SerializeField] float turnSpeed = 0.9f;
     [SerializeField] float moveSpeed = 0.005f;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float speedBoost = 25f;
 
-    // Update is called once per frame
+
     void Update()
     {
         float turnAmount = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -turnAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("You hit something!");
+        moveSpeed = slowSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        // if you hit a speed boost
+        if (other.tag == "Speed Boost")
+        {
+            Debug.Log("Speeeed Booooooooost!");
+            moveSpeed = speedBoost;
+        }
+        // if you hit a slow
+        if (other.tag == "Slow")
+        {
+            Debug.Log("Oh no! You're super slow!");
+            moveSpeed = slowSpeed;
+        }
     }
 }
